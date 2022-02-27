ae38f61b1e9b861b34b34188f828e6909223e36b
asadovfazliddin11@gmail.com <asadovfazliddin11@gmail.com>
2022-02-27 22:52:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("https://qrproject.pythonanywhere.com/products/");
+// fetch("https://qrproject.pythonanywhere.com/products/")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//   fetch("https://qrproject.pythonanywhere.com/products");
+//   const array = document.querySelectorAll(".product");
+//   confirm("https://qrproject.pythonanywhere.com/products/");
+//   for (let i = 0; i < array.length; i++) {
+//     array[i].addEventListener("click", function () {
+//       let id = this.id;
+//       console.log(id);
+//       fetch("https://qrproject.pythonanywhere.com/products/" + id)
+//         .then((response) => response.json())
+//         .then((json) => console.log(json));
+//     });
+//   }
+  
+
